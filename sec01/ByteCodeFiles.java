c7275b88a9995ab0bb5f9903bf14f20ac8f621d1
unknown <yooung513@gmail.com>
2024-04-24 16:37:59 +0900

    1. 자바 시작하기

---
+++ sec01/ByteCodeFiles.java
@@ -0,0 +1,17 @@
+package sec01;
+
+class A {}
+
+class B {}
+
+class C {
+	class D {
+		// C$D.class로 클래스 파일 생성됨
+	}
+}
+
+public class ByteCodeFiles {
+	public static void main(String[] args) {
+		
+	}
+}
